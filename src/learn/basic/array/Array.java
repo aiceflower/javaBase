29bdb8d5a6cfcf14c3efe9f42c6bb3dac5a13c7e
aiceflower <892604768@qq.com>
2017-06-18 22:22:29 +0800

    快速的连接两个数组

--- src/learn/basic/array/Array.java
+++ src/learn/basic/array/Array.java
@@ -1,21 +1,11 @@
 package learn.basic.array;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
-public class Array<T>{
-
-	private Class<T> type;
-
-	public Array(Class<T> type) {
-		this.type = type;
-	}
-
-	@SuppressWarnings("unchecked")
-	private T[] createArray(int size) {
-		return (T[]) java.lang.reflect.Array.newInstance(type, size);
-	}
+public class Array{
 
 	/**
 	 * 数组转换为集合,可以继续添加新的元素
@@ -42,16 +32,36 @@ public class Array<T>{
 	/**
 	 * 数组扩容,默认扩1
 	 */
-	public T[] arraycopy(T[] t) {
+	public static <T> T[] arraycopy(T[] t) {
 		return arraycopy(t, 1);
 	}
 	
 	/**
 	 * 数组扩容，扩指定大小
 	 */
-	public T[] arraycopy(T[] t,int addLength) {
-		T[] array = createArray(t.length + addLength);
+	@SuppressWarnings("unchecked")
+	public static <T> T[] arraycopy(T[] t,int addLength) {
+		T[] array = (T[]) java.lang.reflect.Array.newInstance(t.getClass().getComponentType(), t.length + addLength);
 		System.arraycopy(t, 0, array, 0, t.length);
+		return array;
+	}
+	
+	/**
+	 * 将两个数组合到一起
+	 * @param front
+	 * @param back
+	 * @return
+	 */
+	@SuppressWarnings("unchecked")
+	public static <T> T[] concatenate(T[] front,T[] back){
+		/*
+		 * 使用泛型的方式，但有一个缺点，泛型的方式不适用于基本数据类型int,boolean...
+		 */
+		int fLen = front.length;
+		int bLen = back.length;
+		T[] array = (T[]) java.lang.reflect.Array.newInstance(front.getClass().getComponentType(), fLen + bLen);
+		System.arraycopy(front, 0, array, 0, fLen);
+		System.arraycopy(back, 0, array, fLen, bLen);
 		return array;
 	}
 }
